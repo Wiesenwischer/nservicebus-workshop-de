04100ea833b332df6bb3dc881633fd53eed1a2e3
Marcus Dammann <m.dammann@ams-erp.com>
2022-08-29 13:44:42 +0200

    Initial solution including Step 3 of TODOs

---
+++ src/Microservices-Templates/NServiceBusEndpoint/Program.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using Microsoft.Extensions.Logging;
+
+namespace NServiceBusEndpoint
+{
+    static class Program
+    {
+        private const string EndpointName = "NServiceBusEndpoint";
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseConsoleLifetime()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .UseNServiceBus(ctx =>
+                {
+                    // TODO: consider moving common endpoint configuration into a shared project
+                    // for use by all endpoints in the system
+
+                    var endpointConfiguration = new EndpointConfiguration(EndpointName);
+
+                    endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+
+                    endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+
+                    // TODO: remove this condition after choosing a transport, persistence and deployment method suitable for production
+                    if (Environment.UserInteractive && Debugger.IsAttached)
+                    {
+                        // TODO: choose a durable transport for production
+                        // https://docs.particular.net/transports/
+                        var transportExtensions = endpointConfiguration.UseTransport<LearningTransport>();
+                        transportExtensions.StorageDirectory(".learningtransport");
+
+                        // TODO: choose a durable persistence for production
+                        // https://docs.particular.net/persistence/
+                        endpointConfiguration.UsePersistence<LearningPersistence>();
+
+                        // TODO: create a script for deployment to production
+                        endpointConfiguration.EnableInstallers();
+                    }
+
+                    // TODO: replace the license.xml file with your license file
+
+                    return endpointConfiguration;
+                });
+        }
+
+        static async Task OnCriticalError(ICriticalErrorContext context)
+        {
+            // TODO: decide if stopping the endpoint and exiting the process is the best response to a critical error
+            // https://docs.particular.net/nservicebus/hosting/critical-errors
+            try
+            {
+                await context.Stop();
+            }
+            finally
+            {
+                FailFast($"Critical error, shutting down: {context.Error}", context.Exception);
+            }
+        }
+
+        static void FailFast(string message, Exception exception)
+        {
+            try
+            {
+                // TODO: decide what kind of last resort logging is necessary
+                // TODO: when using an external logging framework it is important to flush any pending entries prior to calling FailFast
+                // https://docs.particular.net/nservicebus/hosting/critical-errors#when-to-override-the-default-critical-error-action
+            }
+            finally
+            {
+                Environment.FailFast(message, exception);
+            }
+        }
+    }
+}
