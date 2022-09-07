756f5e775d9eeda5dba473ee2d84a2c67c5be366
Marcus Dammann <m.dammann@ams-erp.com>
2022-09-07 20:58:38 +0200

    Add message assembly for sales

---
+++ src/Showcase/Sales/Sales.Messages/Commands/PlaceOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace Sales.Messages.Commands
+{
+    public class PlaceOrder
+    {
+        public string OrderId { get; set; }
+        public string ClientId { get; set; }
+        public string ProductId { get; set; }
+    }
+}
