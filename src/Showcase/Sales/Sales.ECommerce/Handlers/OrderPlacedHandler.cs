6af64cdf11defad8072da6c48aa11d4a25b181dd
Marcus Dammann <m.dammann@ams-erp.com>
2022-09-08 08:45:41 +0200

    Adding feedback for order received

---
+++ src/Showcase/Sales/Sales.ECommerce/Handlers/OrderPlacedHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using NServiceBus;
+using Sales.ECommerce.Hubs;
+using Sales.Messages.Events;
+
+namespace Sales.ECommerce.Handlers
+{
+    public class OrderPlacedHandler :
+        IHandleMessages<OrderPlaced>
+    {
+        private IHubContext<ProductsHub> ordersHubContext;
+
+        public OrderPlacedHandler(IHubContext<ProductsHub> ordersHubContext)
+        {
+            this.ordersHubContext = ordersHubContext;
+        }
+
+        public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+        {
+            return ordersHubContext.Clients.Client(message.ClientId).SendAsync("OrderReceived", message.OrderId);
+        }
+    }
+}
